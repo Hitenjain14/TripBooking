8a4ec0c50a0aace7d2fa971614ed3c326a96435e
Hiten Jain <hitenjain146@gmail.com>
2022-01-20 03:11:11 +0530

    Signature changes 1

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -52,13 +52,17 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
-  await Booking.create({ tour, user, price });
+  try {
+    const tour = session.client_reference_id;
+    const price = session.amount_total;
+    const user = (await User.findOne({ email: session.customer_email })).id;
+    await Booking.create({ tour, user, price });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
 
   let event;
@@ -73,7 +77,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+    await createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
 };
