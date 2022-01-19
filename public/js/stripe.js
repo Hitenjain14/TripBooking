866afee228109dc059b08a17697081f6d2ce75bc
Hiten Jain <hitenjain146@gmail.com>
2022-01-19 20:27:01 +0530

    first commit

---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51KJR6uSGNzLgk2Pb54i9y9IB2sHy7OytxjM6brlB2UEytbPOJzNoAhjxjwbm0Oa6wjtB85KEmMrmyFFlmo7un8ex00Vp0M4kSV'
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    //1) Get checkout session from API
+    const session = await axios({
+      method: 'GET',
+      url: `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
+    });
+    console.log(session);
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err.response.data.message);
+  }
+};
